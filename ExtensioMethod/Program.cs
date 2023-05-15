60add87ddb4f24454618374b941386ed2ab218d1
unknown <alesfluiz@gmail.com>
2023-05-14 22:46:50 -0300

    first commit

---
+++ ExtensioMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 05, 14, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
